9ba678c17d93fbe1e786f42e71c567432329d3dc
layme <1149319668@qq.com>
2017-08-29 01:40:21 +0800

    first

---
+++ src/com/jdbctool/constant/SystemInfo.java
@@ -0,0 +1,32 @@
+package com.jdbctool.constant;
+
+public class SystemInfo {
+    public static final String DATABASE_ENVIRONMENT_PRODUCE = "数据库环境：生产";
+    public static final String DATABASE_ENVIRONMENT_DEVELOP = "数据库环境：测试";
+
+    public static final String DATABASE_CONNECTION_ERROR = "数据连接发生错误";
+    public static final String DATABASE_CONNECTION_SUCCESS = "数据连接成功";
+
+    public static final String DATABASE_COMMIT_ERROR = "数据提交失败";
+    public static final String DATABASE_COMMIT_SUCCESS = "数据提交成功";
+
+    public static final String DATABASE_ROLLBACK_ERROR = "数据回滚失败";
+    public static final String DATABASE_ROLLBACK_SUCCESS = "数据回滚成功";
+
+    public static final String DATABASE_CLOSE_ERROR = "数据库连接关闭失败";
+    public static final String DATABASE_CLOSE_SUCCESS = "数据库连接关闭成功";
+
+    public static final String DATABASE_INSERT_ERROR = "数据库插入失败";
+    public static final String DATABASE_INSERT_SUCCESS = "数据库插入成功";
+
+    public static final String DATABASE_DELETE_ERROR = "数据库删除失败";
+    public static final String DATABASE_DELETE_SUCCESS = "数据库删除成功";
+
+    public static final String DATABASE_UPDATE_ERROR = "数据库更新失败";
+    public static final String DATABASE_UPDATE_SUCCESS = "数据库更新成功";
+
+    public static final String DATABASE_SELECT_ERROR = "数据库查询失败";
+    public static final String DATABASE_SELECT_SUCCESS = "数据库查询成功";
+
+    public static final String UNKNOWN_ERROR = "发生未知错误";
+}
