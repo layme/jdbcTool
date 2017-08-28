47fc992579f9297080127bc49e651b4d81014fd2
layme <1149319668@qq.com>
2017-08-29 01:55:39 +0800

    sql

---
+++ src/com/jdbctool/constant/SqlPool.java
@@ -0,0 +1,10 @@
+package com.jdbctool.constant;
+
+public class SqlPool {
+    public static final String ALL_COUNT = "";
+    public static final String SUCCESS_COUNT = "";
+    public static final String ERROR_COUNT = "";
+    public static final String PUB_COUNT = "";
+    public static final String _COUNT = "";
+
+}
