ad8b95abae5f74ac982eb66665dd84e1f74f15ee
layme <1149319668@qq.com>
2017-08-30 23:35:58 +0800

    去除不要的引用

--- src/main/java/com/jdbctool/untils/XmlParse.java
+++ src/main/java/com/jdbctool/untils/XmlParse.java
@@ -1,15 +1,12 @@
 package com.jdbctool.untils;
 
 import com.jdbctool.constant.SystemInfo;
-import com.jdbctool.dbcon.DbConnection;
 import org.dom4j.Document;
-import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 /**
@@ -20,6 +17,8 @@ public class XmlParse {
 
     private Document document = null;
     private static XmlParse xmlParse = null;
+
+    private XmlParse() {}
 
     public static XmlParse getInstance() {
         if (null == xmlParse) {
@@ -40,8 +39,8 @@ public class XmlParse {
     }
 
     public String getSql(String id) throws IllegalArgumentException {
-        String sql = null;
-        List<Element> list = document.selectNodes("/sqls/sql[@id='" + id + "']");
+        String sql;
+        List<org.dom4j.Element> list = document.selectNodes("/sqls/sql[@id='" + id + "']");
         if (null == list || list.size() < 1) {
             throw new IllegalArgumentException(SystemInfo.ID_NOT_FOUND);
         }
