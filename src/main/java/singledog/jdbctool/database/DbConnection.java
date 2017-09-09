fa1ce7e796397f33755e264c86e17906bc8302e4
layme <1149319668@qq.com>
2017-09-09 17:01:08 +0800

    新架构

---
+++ src/main/java/singledog/jdbctool/database/DbConnection.java
@@ -0,0 +1,62 @@
+package singledog.jdbctool.database;
+
+import singledog.jdbctool.configuration.Configuration;
+import singledog.jdbctool.configuration.DataSource;
+import singledog.jdbctool.constant.SystemInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.util.ResourceBundle;
+
+public class DbConnection {
+    private static final Logger log = LoggerFactory.getLogger(DbConnection.class);
+
+    private DataSource dataSource;  // 数据库参数
+
+    /**
+     * 构造函数
+     * @param dataSource
+     */
+    protected DbConnection(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    /**
+     * 构建执行器 默认关闭自动提交
+     * @return executor
+     * @throws Exception
+     */
+    public Executor buildExecutor() throws Exception {
+        return this.buildExecutor(false);
+    }
+
+    /**
+     * 构建执行器
+     * @param autoCommit
+     * @return executor
+     * @throws Exception
+     */
+    public Executor buildExecutor(boolean autoCommit) throws Exception {
+        log.debug(SystemInfo.DATABASE_ENVIRONMENT + dataSource);
+        try {
+            Class.forName(dataSource.getDriver());
+            Connection connection = DriverManager.getConnection(dataSource.getUrl(),
+                    dataSource.getUrl(),
+                    dataSource.getPassword());
+            connection.setAutoCommit(autoCommit);
+            log.debug(SystemInfo.DATABASE_CONNECTION_SUCCESS);
+            return new Executor(connection);
+        } catch (SQLException e) {
+            log.debug(SystemInfo.DATABASE_CONNECTION_ERROR);
+            throw e;
+        } catch (Exception e) {
+            log.debug(SystemInfo.UNKNOWN_ERROR);
+            throw e;
+        }
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+}
