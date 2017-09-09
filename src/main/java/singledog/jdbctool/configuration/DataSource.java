32eeb923dd851e95cfa4f287e427b1101bb7bcf7
layme <1149319668@qq.com>
2017-09-09 19:37:53 +0800

    20170909

--- src/main/java/singledog/jdbctool/configuration/DataSource.java
+++ src/main/java/singledog/jdbctool/configuration/DataSource.java
@@ -4,25 +4,18 @@ package singledog.jdbctool.configuration;
  * Created by admin on 2017/9/8.
  */
 public class DataSource {
-    private String profile;  // 环境标签
     private String driver;  // 驱动
     private String url;  // 地址
     private String username;  // 用户
     private String password;  // 密码
+
+    public DataSource() {}
 
     public DataSource(String driver, String url, String username, String password) {
         this.driver = driver;
         this.url = url;
         this.username = username;
         this.password = password;
-    }
-
-    public String getProfile() {
-        return profile;
-    }
-
-    public void setProfile(String profile) {
-        this.profile = profile;
     }
 
     public String getDriver() {
