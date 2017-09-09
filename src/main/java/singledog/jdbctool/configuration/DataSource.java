407c6ffbb9764dd50e50b7139023c075431510ab
layme <1149319668@qq.com>
2017-09-09 17:07:22 +0800

    数据库参数类

---
+++ src/main/java/singledog/jdbctool/configuration/DataSource.java
@@ -0,0 +1,59 @@
+package singledog.jdbctool.configuration;
+
+/**
+ * Created by admin on 2017/9/8.
+ */
+public class DataSource {
+    private String profile;  // 环境标签
+    private String driver;  // 驱动
+    private String url;  // 地址
+    private String username;  // 用户
+    private String password;  // 密码
+
+    public DataSource(String driver, String url, String username, String password) {
+        this.driver = driver;
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
